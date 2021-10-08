79418a9c2623471515930acd97c51934c4049ed2
Vivek Jaiswal <vivekjaiswal@live.in>
2021-10-08 21:10:13 +0530

    Google LLD round logger problem

--- logger/src/com/vivek/logger/Driver.java
+++ logger/src/com/vivek/logger/Driver.java
@@ -2,8 +2,27 @@ package com.vivek.logger;
 
 public class Driver {
 
+    /*
+     * {3} started at {7} and ended at {19}
+     * {2} started at {8} and ended at {12}
+     * {1} started at {12} and ended at {15}
+     */
     public static void main(String[] args) {
-
+        final LogClient logger = new LoggerImplementation();
+        logger.start("1", 1);
+        logger.poll();
+        logger.start("3", 2);
+        logger.poll();
+        logger.end("1");
+        logger.poll();
+        logger.start("2", 3);
+        logger.poll();
+        logger.end("2");
+        logger.poll();
+        logger.end("3");
+        logger.poll();
+        logger.poll();
+        logger.poll();
     }
 
 }
