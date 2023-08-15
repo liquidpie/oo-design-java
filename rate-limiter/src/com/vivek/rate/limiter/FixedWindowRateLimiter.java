dd47b57a31243531a21cf417a71f04a93c624dc5
liquidpie <vivekjaiswal@live.in>
2023-08-15 23:55:22 +0530

    rate limiter problem

--- rate-limiter/src/com/vivek/rate/limiter/FixedWindowRateLimiter.java
+++ rate-limiter/src/com/vivek/rate/limiter/FixedWindowRateLimiter.java
@@ -1,63 +1,70 @@
 package com.vivek.rate.limiter;
 
-import java.time.Instant;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Fixed Window Algorithm:
- * The Fixed Window Algorithm is the simplest rate limiting algorithm.
- * It limits the rate by counting the number of requests made within a fixed time window.
- * If the number of requests exceeds the limit, then any subsequent requests are blocked until the start of the next window.
- * For example, if the limit is set to 10 requests per second,
- * then the algorithm counts the number of requests made in the last second and blocks any additional requests until the start of the next second.
  *
- * In the below code, we have a FixedWindowRateLimiter class that implements the Fixed Window Algorithm for rate limiting.
- * It takes the maximum number of requests allowed within a time window (limit) and the duration of the time window in seconds (windowSeconds)
- * as parameters in the constructor.
+ * Fixed Window Rate Limiting works by dividing time into fixed windows and allowing a certain number of requests in each window.
+ * For example, if we have a limit of 100 requests per hour, and our window starts at 2:00, we can make 100 requests between 2:00 and 3:00.
  *
- * The allowRequest() method is used to check if a request is allowed or not. It synchronizes the method to ensure thread safety.
+ * However, if we make all 100 requests at 2:10, we will have to wait until 3:00 to make another request.
+ * This can lead to uneven distribution of requests, with potential for bursts of traffic at the start of each window.
  *
- * The clearExpiredRequests() method is called before checking the request queue for the number of requests.
- * It clears any expired requests from the queue based on the current time and the window duration.
- * The expired requests are removed from the queue to keep track of only the requests within the defined time window.
- *
- * You can create an instance of FixedWindowRateLimiter and use the allowRequest() method to control the rate
- * at which requests are allowed within the fixed window.
- *
- * It’s important to note that the Fixed Window Algorithm resets the request count at the start of each time window.
- * This means that the rate limit is enforced strictly within each window,
- * but it doesn’t take into account the distribution of requests within the window.
+ * https://www.codereliant.io/rate-limiting-deep-dive/
  */
 public class FixedWindowRateLimiter {
 
-    private final int limit; // Maximum number of requests allowed within the time window
-    private final int windowSeconds; // Duration of the time window in seconds
-    private final Queue<Instant> requestQueue; // Queue to store request timestamps
+    private final int maxRequestPerWindow;
+    private final long windowSizeInMillis;
+    private final Map<String, Window> store;
 
-    public FixedWindowRateLimiter(int limit, int windowSeconds) {
-        this.limit = limit;
-        this.windowSeconds = windowSeconds;
-        this.requestQueue = new ArrayDeque<>();
+    public FixedWindowRateLimiter(int maxRequestPerWindow, long windowSizeInMillis) {
+        this.maxRequestPerWindow = maxRequestPerWindow;
+        this.windowSizeInMillis = windowSizeInMillis;
+        this.store = new HashMap<>();
     }
 
-    public synchronized boolean allowRequest() {
-        clearExpiredRequests(); // Clear expired requests from the queue
+    public synchronized boolean isAllowed(String clientId) {
+        long currentTimeMillis = System.currentTimeMillis();
+        Window window = store.get(clientId);
 
-        if (requestQueue.size() < limit) {
-            requestQueue.offer(Instant.now()); // Add current request timestamp to the queue
-            return true; // Request is allowed
-        } else {
+        // If the window doesn't exist or has expired, create a new window
+        if (window == null || window.getStartTime() < currentTimeMillis - windowSizeInMillis) {
+            window = new Window(currentTimeMillis, 0);
+        }
+
+        // Check if the number of requests in the window exceeds the maximum allowed
+        if (window.getRequestCount() >= maxRequestPerWindow) {
             return false; // Request is not allowed
         }
+
+        // Increment the request count and update the window in the store
+        window.setRequestCount(window.getRequestCount() + 1);
+        store.put(clientId, window);
+        return true; // Request is allowed
     }
 
-    private void clearExpiredRequests() {
-        Instant now = Instant.now();
-        Instant windowStart = now.minusSeconds(windowSeconds);
+    private static class Window {
+        private final long startTime;
+        private int requestCount;
 
-        while (!requestQueue.isEmpty() && requestQueue.peek().isBefore(windowStart)) {
-            requestQueue.poll(); // Remove expired requests from the queue
+        public Window(long startTime, int requestCount) {
+            this.startTime = startTime;
+            this.requestCount = requestCount;
+        }
+
+        public long getStartTime() {
+            return startTime;
+        }
+
+        public int getRequestCount() {
+            return requestCount;
+        }
+
+        public void setRequestCount(int requestCount) {
+            this.requestCount = requestCount;
         }
     }
 
